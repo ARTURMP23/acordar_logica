ba6089ef32130cb591650088fce6a5680bbdb0cb
Artur Matos <arturrtutu@gmail.com>
2024-03-18 15:15:16 -0300

    commit inicial;

---
+++ src/tarefaAcordar/logicaAcordar.java
@@ -0,0 +1,31 @@
+package tarefaAcordar;
+public class logicaAcordar {
+
+	public static void main(String[] args) {
+		System.out.print("Acordo;"
+				+"Desligo o despertador;"
+				+ "Levanto da cama;"
+				+ "Abro as janelas;"
+				+ "Saio do quarto;"
+				+ "Entro no banheiro;"
+				+ "Escovo os dentes;"
+				+ "Lavo o rosto;"
+				+ "Saio do banheiro;"
+				+ "Vou para a cozinha;"
+				+ "Separo o café;"
+				+ "Separo a água;"
+				+ "Esquento a água;"
+				+ "Adiciono o café;"
+				+ "Preparo minha xicara;"
+				+ "Arrumo a mesa;"
+				+ "Quando o café estiver pronto: Levo até minha xicara e coloco nela;"
+				+ "Vou até a gaveta;"
+				+ "Pego uma colher;"
+				+ "Retorno a mesa e a minha xicara;"
+				+ "Pego um pouco de açucar e coloco no café;"
+				+ "Misturo o café com o açucar;"
+				+ "Bebo;");
+
+	}
+
+}
